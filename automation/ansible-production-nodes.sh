53979ded66ca6603d043a7cb846ef4141dba9913
Victor Green <thevictorgreen@gmail.com>
2022-01-02 15:38:05 -0500

    initial codebase

---
+++ automation/ansible-production-nodes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# For Public Nodes
+ansible-playbook -i inventory/ec2_external_production.py -u ubuntu --private-key ../credentials/KEY-PAIR-HERE.pem site_ubuntu.yml -e 'ansible_python_interpreter=/usr/bin/python3' -vv
+ansible-playbook -i inventory/ec2_external_production.py -u centos --private-key ../credentials/KEY-PAIR-HERE.pem site_centos.yml -e 'ansible_python_interpreter=/usr/bin/python' -vv
+
+# For Private Nodes
+#ansible-playbook -i inventory/ec2_internal_production.py -u ubuntu --private-key ../credentials/KEY-PAIR-HERE.pem site_ubuntu.yml -e 'ansible_python_interpreter=/usr/bin/python3' -vv
+#ansible-playbook -i inventory/ec2_internal_production.py -u centos --private-key ../credentials/KEY-PAIR-HERE.pem site_centos.yml -e 'ansible_python_interpreter=/usr/bin/python' -vv
